b5d9399eb02616f584c1749c3d057d3a2f63db1e
FerchoChainz <a21110132@ceti.mx>
2026-06-05 17:13:42 -0600

    Adding types and states. Just rendering elements by the moment

--- tipCalculator/src/App.tsx
+++ tipCalculator/src/App.tsx
@@ -1,7 +1,36 @@
+import MenuItem from "./components/MenuItem";
+import OrderContents from "./components/OrderContents";
+import { menuItems } from "./data/db";
+import useOrder from "./hooks/useOrder";
+
 function App() {
+  const { order, addItem } = useOrder();
   return (
-    <div>App</div>
-  )
+    <>
+      <header className="bg-teal-400 py-5">
+        <h1 className="text-center text-4xl font-black">Tip Calculator</h1>
+      </header>
+
+      <main className="max-w-7xl mx-auto py-20 grid md: grid-cols-2">
+        <div className="p-5">
+          <h2 className="text-xl font-black">Menu</h2>
+
+          <div className="space-y-3 mt-10">
+            {menuItems.map((item) => (
+              <MenuItem key={item.id} item={item} addItem={addItem} />
+            ))}
+          </div>
+        </div>
+
+        <div className="p-5 border-dashed border-slate-400 rounded-lg space-y-10">
+          <OrderContents
+          order={order} 
+          
+          />
+        </div>
+      </main>
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ tipCalculator/src/components/MenuItem.tsx
@@ -0,0 +1,22 @@
+import type { MenuItem } from "../types";
+
+type MenuItemProps = {
+  item: MenuItem;
+  addItem: (item: MenuItem) => void;
+};
+
+function MenuItem({ item, addItem }: MenuItemProps) {
+  return (
+    <>
+      <button
+        className="w-full flex items-center justify-between border-2 border-gray-300 rounded-lg p-4 mb-4 hover:bg-gray-100 transition-colors hover:border-gray-400"
+        onClick={() => addItem(item)}
+      >
+        <p>{item.name}</p>
+        <p className="font-black">${item.price}</p>
+      </button>
+    </>
+  );
+}
+
+export default MenuItem;
