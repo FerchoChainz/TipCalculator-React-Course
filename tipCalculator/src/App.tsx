c5fff01be0e72dbf2ada26b762db8aad8f4be5d4
FerchoChainz <a21110132@ceti.mx>
2026-06-22 14:56:54 -0600

    Calculate the total amount of the bill

--- tipCalculator/src/App.tsx
+++ tipCalculator/src/App.tsx
@@ -2,6 +2,7 @@ import MenuItem from "./components/MenuItem";
 import OrderContents from "./components/OrderContents";
 import { menuItems } from "./data/db";
 import useOrder from "./hooks/useOrder";
+import OrderTotals from "./components/OrderTotals";
 
 function App() {
   const { order, addItem, removeItem } = useOrder();
@@ -28,6 +29,12 @@ function App() {
           removeItem={removeItem}
           
           />
+
+          <OrderTotals
+          order={order}
+          
+          />
+
         </div>
       </main>
     </>
---
+++ tipCalculator/src/components/OrderTotals.tsx
@@ -0,0 +1,40 @@
+import { useMemo } from "react"
+import type { OrderItem } from "../types"
+import { formatCurrency } from "../helpers"
+
+type OrderTotalsProps ={
+    order: OrderItem[]
+}
+
+function OrderTotals({order} : OrderTotalsProps) {
+
+    const subtotalAmount = useMemo(()=> {
+      return order.reduce((total, item) => total + (item.price * item.quantity), 0)  
+    }, [order])
+
+
+  return (
+    <>
+    <div className="space-y-3">
+        <h2 className="font-black text-2xl">Totales y Propinas</h2>
+
+        <p>Subtotal a pagar: {''}
+            <span className="font-bold">{formatCurrency(subtotalAmount)}</span>
+        </p>
+
+        <p>Propina: {''}
+            <span className="font-bold">$0</span>
+        </p>
+
+        <p>Total a pagar: {''}
+            <span className="font-bold">$0</span>
+        </p>
+    </div>
+
+    <button></button>
+
+    </>
+  )
+}
+
+export default OrderTotals
