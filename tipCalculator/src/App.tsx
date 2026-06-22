9ff6dd9892f290870342924c49fa6ea72497d7ab
FerchoChainz <a21110132@ceti.mx>
2026-06-22 15:43:41 -0600

    Adding tip quantity selected

--- tipCalculator/src/App.tsx
+++ tipCalculator/src/App.tsx
@@ -3,9 +3,10 @@ import OrderContents from "./components/OrderContents";
 import { menuItems } from "./data/db";
 import useOrder from "./hooks/useOrder";
 import OrderTotals from "./components/OrderTotals";
+import TipPercentageForm from "./components/TipPercentageForm";
 
 function App() {
-  const { order, addItem, removeItem } = useOrder();
+  const { order, addItem, removeItem, tip, setTip } = useOrder();
   return (
     <>
       <header className="bg-teal-400 py-5">
@@ -28,6 +29,10 @@ function App() {
           order={order} 
           removeItem={removeItem}
           
+          />
+
+          <TipPercentageForm 
+          setTip={setTip}
           />
 
           <OrderTotals
