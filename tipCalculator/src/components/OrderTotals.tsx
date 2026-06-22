ef1fbffa3209657bcc2bdfb1759a2d6cc617f7b2
FerchoChainz <a21110132@ceti.mx>
2026-06-22 15:51:50 -0600

    Adding total amount + tips

--- tipCalculator/src/components/OrderTotals.tsx
+++ tipCalculator/src/components/OrderTotals.tsx
@@ -3,14 +3,18 @@ import type { OrderItem } from "../types"
 import { formatCurrency } from "../helpers"
 
 type OrderTotalsProps ={
-    order: OrderItem[]
+    order: OrderItem[],
+    tip: number
 }
 
-function OrderTotals({order} : OrderTotalsProps) {
+function OrderTotals({order, tip} : OrderTotalsProps) {
 
     const subtotalAmount = useMemo(()=> {
       return order.reduce((total, item) => total + (item.price * item.quantity), 0)  
     }, [order])
+
+    const tipAmount = useMemo(() => subtotalAmount * tip, [tip, order])
+    const totalAmount = useMemo(() => subtotalAmount + tipAmount, [tip, order])
 
 
   return (
@@ -23,11 +27,11 @@ function OrderTotals({order} : OrderTotalsProps) {
         </p>
 
         <p>Propina: {''}
-            <span className="font-bold">$0</span>
+            <span className="font-bold">{formatCurrency(tipAmount)}</span>
         </p>
 
-        <p>Total a pagar: {''}
-            <span className="font-bold">$0</span>
+        <p>Total a pagar: {''}  
+            <span className="font-bold">{formatCurrency(totalAmount)}</span>
         </p>
     </div>
 
